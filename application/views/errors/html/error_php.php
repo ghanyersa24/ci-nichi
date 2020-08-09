0e1b2ff2dd589e2f1631546127c51948ccb7e868
Ghany Abdillah Ersa <ghany.ersa@bigio.id>
2020-08-09 15:12:39 +0700

    replace error sql to json

--- application/views/errors/html/error_php.php
+++ application/views/errors/html/error_php.php
@@ -1,8 +1,10 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
+if (REST_API)
+	error($message);
 ?>
 
-<div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
+<!-- <div style="border:1px solid #990000;padding-left:20px;margin:0 0 10px 0;">
 
 <h4>A PHP Error was encountered</h4>
 
@@ -11,12 +13,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <p>Filename: <?php echo $filepath; ?></p>
 <p>Line Number: <?php echo $line; ?></p>
 
-<?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === TRUE): ?>
+<?php if (defined('SHOW_DEBUG_BACKTRACE') && SHOW_DEBUG_BACKTRACE === TRUE) : ?>
 
 	<p>Backtrace:</p>
-	<?php foreach (debug_backtrace() as $error): ?>
+	<?php foreach (debug_backtrace() as $error) : ?>
 
-		<?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0): ?>
+		<?php if (isset($error['file']) && strpos($error['file'], realpath(BASEPATH)) !== 0) : ?>
 
 			<p style="margin-left:10px">
 			File: <?php echo $error['file'] ?><br />
@@ -30,4 +32,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <?php endif ?>
 
-</div>
+</div> -->
